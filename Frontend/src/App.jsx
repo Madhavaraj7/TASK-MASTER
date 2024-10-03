b8e59c26714766940a0040a8f1117c5741e869f3
Madhavaraj R <raj266864@gmail.com>
2024-10-03 12:45:41 +0530

    completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider, useAuth } from "./context/AuthContext"; 
 
+
 const App = () => {
   return (
     <AuthProvider>
